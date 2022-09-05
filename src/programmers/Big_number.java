46474dc9aa2ca77b04945578387e840600fbe5e9
xogml9704 <xogml9704@naver.com>
2022-09-05 20:09:15 +0900

    practcie

--- src/programmers/Big_number.java
+++ src/programmers/Big_number.java
@@ -2,22 +2,13 @@ package programmers;
 
 public class Big_number {
 	public static void main(String[] args) {
-		int[] numbers = {6, 10, 2};
-		// int[] numbers = {3, 30, 34, 5, 9};
-		String arr[] = new String[numbers.length];
+		// int[] numbers = {6, 10, 2};
+		int[] numbers = {3, 30, 34, 5, 9};
+		
+		String[] arr = new String[numbers.length];
+		
 		for(int i=0; i<numbers.length; i++) {
 			arr[i] = String.valueOf(numbers[i]);
-		}
-		
-		String[][] a = new String[numbers.length][2];
-		int[][] b = new int[numbers.length][2];
-		for(int i=0; i<arr.length; i++) {
-			a[i] = arr[i].split("");
-			b[i][0] = Integer.parseInt(a[i][0]);
-		}
-		
-		for(int i=0; i<a.length; i++) {
-			
 		}
 		
 	}
