23c11b36476a03b9b8ebf01209d42768ea5c6817
xogml9704 <xogml9704@naver.com>
2022-09-06 12:22:37 +0900

    practcie

--- src/programmers/Big_number.java
+++ src/programmers/Big_number.java
@@ -6,9 +6,22 @@ public class Big_number {
 		int[] numbers = {3, 30, 34, 5, 9};
 		
 		String[] arr = new String[numbers.length];
+		String tmp;
 		
 		for(int i=0; i<numbers.length; i++) {
 			arr[i] = String.valueOf(numbers[i]);
+		}
+		
+		for(int i=0; i<numbers.length; i++) {
+			for(int j=i+1; j<numbers.length; j++) {
+				if(arr[i].charAt(0) < arr[j].charAt(0)) {
+					tmp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = tmp;
+				} else if (arr[i].charAt(0)== arr[j].charAt(0)) {
+					
+				}
+			}
 		}
 		
 	}
