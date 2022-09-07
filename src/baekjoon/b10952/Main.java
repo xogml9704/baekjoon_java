4ceb79e73365bf5db331e80ffeeaa0a996a1d381
xogml9704 <xogml9704@naver.com>
2022-09-07 15:29:41 +0900

    practcie

--- src/baekjoon/b10952/Main.java
+++ src/baekjoon/b10952/Main.java
@@ -1,11 +1,29 @@
 package baekjoon.b10952;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+    	// 1 1  2
+    	// 2 3  5
+    	// 3 4  7
+    	// 9 8  17
+    	// 5 2  7
+    	// 0 0  종료
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	StringTokenizer st; // 문자열을 잘라서 넣어줌
     	
-    	for(int i=0; i<)
+    	while(true) {
+    		st = new StringTokenizer(br.readLine());
+    		bw.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"\n");
+    		System.out.println(br.read());
+    	}
+    	// bw.close();
     }
 }
--- src/baekjoon/b15552/Main.java
+++ src/baekjoon/b15552/Main.java
@@ -1,15 +1,24 @@
 package baekjoon.b15552;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int a = br.read();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 		
-		System.out.println(a);
-		System.out.println(a);
+		int t = Integer.parseInt(br.readLine());
+		for(int i=0; i<t; i++) {
+			st = new StringTokenizer(br.readLine());
+			bw.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"\n");
+		}
+		
+		bw.close();
 	}
 }
