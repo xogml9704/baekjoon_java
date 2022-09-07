f7b73ef105dfc959a394f379d56f9e46ff424aeb
xogml9704 <xogml9704@naver.com>
2022-09-07 21:00:43 +0900

    practcie

---
+++ src/programmers/Darts.java
@@ -0,0 +1,53 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Darts {
+	public static void main(String[] args) {
+		int[] numbers = {2, 1, 3, 4, 1};
+		
+		int numbers_length = 0;
+		
+		for(int i=0; i<numbers.length; i++) {
+			numbers_length += i;
+		}
+		
+		int[] result = new int[numbers_length];
+		
+		int k = 0;
+		for(int i=0; i<numbers.length; i++) {
+			for(int j=i+1; j<numbers.length; j++) {
+				result[k] = numbers[i] + numbers[j];
+				k++;
+			}
+		}
+		
+		Arrays.sort(result);
+		
+		int count = 0;
+		
+		for(int i=0; i<result.length; i++) {
+			for(int j=i+1; j<result.length; j++) {
+				if(result[i] == result[j] && result[i] != 0) {
+					count++;
+					result[j] = 0;
+				}
+			}
+		}
+		int sum = result.length - count;
+		int[] arr = new int[sum];
+		
+		int arr_result =0;
+		for(int i=0; i<result.length; i++) {
+			if(result[i] != 0) {
+				arr[arr_result] = result[i];
+				arr_result++;
+			}
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ,");
+		}
+		
+	}
+}
