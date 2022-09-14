4b810b905c64777aba82e71f305e3e2ae0427e7c
xogml9704 <xogml9704@naver.com>
2022-09-14 12:22:29 +0900

    practcie

--- src/baekjoon/b4344/Main.java
+++ src/baekjoon/b4344/Main.java
@@ -7,9 +7,20 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int a = Integer.parseInt(sc.nextLine());
+		long finish = 0;
 		
 		for(int i=0; i<a; i++) {
-			
+			String[] arr = sc.nextLine().split(" ");
+			long[] arr2 = new long[arr.length];
+			for(int j=0; j<arr.length; j++) {
+				arr2[j] = Integer.parseInt(arr[j]);
+			}
+			for(int j=1; j<arr2.length; j++) {
+				finish += (long) arr2[j];
+				System.out.println(finish);
+			}
+			finish = finish / arr2[0];
 		}
+		System.out.println(finish);
 	}
 }
