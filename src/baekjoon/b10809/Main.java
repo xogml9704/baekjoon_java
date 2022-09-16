48926418639882603750d34891bfa8cb8ab05c2e
xogml9704 <xogml9704@naver.com>
2022-09-16 17:51:24 +0900

    practcie

--- src/baekjoon/b10809/Main.java
+++ src/baekjoon/b10809/Main.java
@@ -6,13 +6,26 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String[] arr = sc.nextLine().split("");
-		char[] arr2 = new char[arr.length];
-		for(int i=0; i<arr.length; i++) {
-			arr2[i] = arr[i].charAt(0);
-			if(arr2[i] > 'Z') {
-				arr2[i] -= 32;
+		String a = sc.nextLine();
+		char[] arr = new char[a.length()];
+		
+		for(int i=0; i<a.length(); i++) {
+			arr[i] = a.charAt(i);
+		}
+		int[] arr2 = new int[26];
+		for(int i=0; i<arr2.length; i++) {
+			arr2[i] = -1;
+		}
+		for(char i = 'a'; i<='z'; i++) {
+			for(int j=0; j<arr.length; j++) {
+				if(i == arr[j]) {
+					arr2[i-97] = (int) j;
+					break;
+				}
 			}
+		}
+		for(int i=0; i<arr2.length; i++) {
+			System.out.print(arr2[i]+" ");
 		}
 	}
 }
---
+++ src/baekjoon/b5622/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.b5622;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int finish = 0;
+		String[] arr = sc.nextLine().split("");
+		
+		for(int i=0; i<arr.length; i++) {
+			int count = 2;
+			
+		}
+	}
+}
