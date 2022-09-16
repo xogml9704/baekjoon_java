38f0a307326ec1044d60e35c49a7d4e77ec6fb21
xogml9704 <xogml9704@naver.com>
2022-09-16 17:18:06 +0900

    practcie

--- src/baekjoon/b10809/Main.java
+++ src/baekjoon/b10809/Main.java
@@ -1,7 +1,18 @@
 package baekjoon.b10809;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		String[] arr = sc.nextLine().split("");
+		char[] arr2 = new char[arr.length];
+		for(int i=0; i<arr.length; i++) {
+			arr2[i] = arr[i].charAt(0);
+			if(arr2[i] > 'Z') {
+				arr2[i] -= 32;
+			}
+		}
 	}
 }
