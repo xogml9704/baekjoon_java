a63c51b8f3500a18cf9173dcfcd972fca12a8511
xogml9704 <xogml9704@naver.com>
2022-09-19 16:46:39 +0900

    practcie

--- src/baekjoon/b5622/Main.java
+++ src/baekjoon/b5622/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int finish = 0;
 		String dial = sc.nextLine();
 		char[] arr = new char[dial.length()];
 		for(int i=0; i<dial.length(); i++) {
@@ -14,18 +13,20 @@ public class Main {
 		int sum = 0;
 		for(int i=0; i<arr.length; i++) {
 			int count = 0;
-			for(char j='A'; j<='Z'; j++) {
-				if(arr[i] == j && arr[i] < 'W') {
-					int a  = j - 'A';
-					count = (a / 3) + 3;
-					break;
-				} else if (arr[i] == j && arr[i] >= 'W') {
-					count = 10;
-					break;
+			if(arr[i] <= 'Z') {
+				for(char j='A'; j<='Z'; j++) {
+					if(arr[i] == j && arr[i] < 'W') {
+						int a  = j - 'A';
+						count = (a / 3) + 3;
+						break;
+					} else if (arr[i] == j && arr[i] >= 'W' && arr[i] <= 'Z') {
+						count = 10;
+						break;
+					}
 				}
+				
 			}
 			sum += count;
-			System.out.println(count);
  		}
 		System.out.println(sum);
 	}
