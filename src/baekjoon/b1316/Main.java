e5536cbc1fdaecb23bbe1c67460e2714bb7d6066
xogml9704 <xogml9704@naver.com>
2022-09-20 19:14:29 +0900

    practcie

---
+++ src/baekjoon/b1316/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.b1316;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = Integer.parseInt(sc.nextLine());
+		
+		for(int i=0; i<a; i++) {
+			String[] arr = sc.nextLine().split("");
+			for(int j=0; j<arr.length; j++) {
+				
+			}
+		}
+	}
+}
