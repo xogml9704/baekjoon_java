4d34a490a027942bbbebf8fce6f85b4348e3b75e
xogml9704 <xogml9704@naver.com>
2022-09-22 17:36:12 +0900

    practcie

--- src/programmers/p12906/Main.java
+++ src/programmers/p12906/Main.java
@@ -1,22 +1,19 @@
 package programmers.p12906;
 
-import java.util.Arrays;
-
 public class Main {
 	public static void main(String[] args) {
 		int[] arr = {4,4,4,3,3};
-		String a = "";
+		int j = 0;
+		int[] temp = new int[arr.length];
 		for(int i=0; i<arr.length-1; i++) {
 			if(arr[i] != arr[i+1]) {
-				a += arr[i];
+				temp[j++] += arr[i];
 			}
 		}
-		a += arr[arr.length-1];
-		String[] arr2 = a.split("");
-		int[] finish = new int[arr2.length];
-		for(int i=0; i<finish.length; i++) {
-			finish[i] = Integer.parseInt(arr2[i]);
+		temp[j++] = arr[arr.length-1];
+		int[] answer = new int[j];
+		for (int i=0; i<j; i++) {
+			answer[i] = temp[i];
 		}
-		
 	}
 }
