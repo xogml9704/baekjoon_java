e16f4c782dc461f68c697a66dfff93ccc43501e7
xogml9704 <xogml9704@naver.com>
2022-10-12 16:29:21 +0900

    practcie

--- src/programmers/p131128/Main2.java
+++ src/programmers/p131128/Main2.java
@@ -1,21 +1,43 @@
 package programmers.p131128;
 
+import java.util.Arrays;
+
 public class Main2 {
     public static void main(String[] args) {
+        // String X = "100";
+        // String Y = "2345";
+        
+        //String X = "100";
+        //String Y = "23400";
+        
         String X = "100";
-        String Y = "23400";
+        String Y = "123450";
         
         String[] arrX = X.split("");
         String[] arrY = Y.split("");
-        int countX = 0;
-        int countY = 0;
+        boolean Z = true;
+        String answer = "";
+        int[] arrInt = new int[10];
         for(int i=0; i<arrX.length; i++) {
-            for(int j=0; j<10; j++) {
-                if(arrX[i].equals(String.valueOf(j))) {
-                    countX++;
+            for(int j=0; j<arrY.length; j++) {
+                if(arrX[i].equals(arrY[j])) {
+                    Z = false;
+                    arrInt[Integer.parseInt(arrY[j])]++;
+                    arrY[j] = null;
+                    break;
                 }
-                System.out.println(countX);
             }
         }
+        if(Z) {
+            answer = "-1";
+        } else {
+            for(int i=9; i>=0; i--) {
+                for(int j=0; j<arrInt[i]; j++) {
+                    answer += i;
+                }
+            }
+        }
+        System.out.println(answer);
     }
 }
+
